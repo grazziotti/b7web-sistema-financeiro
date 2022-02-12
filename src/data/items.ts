20f67f7643c4c36914b94d95a4d545a0b5247c04
Daniel Grazziotti <dgplays14@gmail.com>
2022-02-12 09:36:23 -0300

    Add: Tabela com dados & Área de informações

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+    { date: new Date(2022, 1, 6), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2022, 1, 11), category: 'food', title: 'Burger King', value: 28 },
+    { date: new Date(2022, 1, 11), category: 'rent', title: 'Aluguel Apt', value: 2300 },
+    { date: new Date(2022, 0, 12), category: 'salary', title: 'Salário ACME', value: 4500 }
+]
